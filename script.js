d3fd0c9f5cde80f619de6682165446d9f77ed18d
Zack Friendly <friendly.wisconsin@gmail.com>
2020-02-07 16:21:43 -0500

    Building object for all dog API data inputs

--- script.js
+++ script.js
@@ -2,26 +2,31 @@ let dog__breed = document.querySelector("#dog__breed");
 let dog__info = document.querySelector("#dog__info");
 let baseUrl = "https://api.thedogapi.com/v1/images/search?breed_ids=";
 
-function dogNameGenerator(e) {
-  e.preventDefault();
-  fetch(baseUrl, {
-    headers: {
-      "x-api-key": "d95fa56a-2c05-4a28-b011-65e14b05b28b"
-    }
-  })
-    .then(res => {
-      console.log("success", res);
-      return res.json();
-    })
-    .then(res => {
-      let dogName = res.name;
-      dog__breed.innerText = dogName;
-      dog__info.innerText = res["bred_for"];
-      console.log(dog__breed);
-    });
-}
+// function dogNameGenerator(e) {
+//   e.preventDefault();
+//   fetch(baseUrl, {
+//     headers: {
+//       "x-api-key": "d95fa56a-2c05-4a28-b011-65e14b05b28b"
+//     }
+//   })
+//     .then(res => {
+//       console.log("success", res);
+//       return res.json();
+//     })
+//     .then(res => {
+//       Corgi.push(
+//         res[0].breeds[0].name,
+//         res[0].breeds[0]["bred_for"],
+//         res[0].breeds[0]["breed.group"],
+//         res[0].breeds[0].temperament,
+//         res[2]
+//       );
+//       console.log(data);
+//       console.log(Corgi);
+//     });
 
 //Fetch to pull all the correct indices for the 10 dog breeds
+const corgi = [];
 let dogLink = document.querySelectorAll(".link");
 let dogArray = [10, 23, 36, 41, 71, 125, 149, 184, 177, 226];
 for (i = 0; i < dogArray.length; i++) {
@@ -35,9 +40,13 @@ for (i = 0; i < dogArray.length; i++) {
       return res.json();
     })
     .then(data => {
+      dog = {};
+      corgi.push(dog);
       console.log(data);
+      console.log(corgi);
     });
 }
+
 //Hamburger slider function
 let navList = document.querySelector(".nav__list");
 let hamburger = document.querySelector(".hamburger");
